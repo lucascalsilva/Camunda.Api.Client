78ca004531c3ca8241d636ea861f2d406cd78ab4
Stefan Wiese <stefan.wiese@camunda.com>
2019-08-15 16:08:39 +0200

    version to 2.4.6-dev and added virtual keyword to some methods in order to test them via NSubstiute

--- Camunda.Api.Client/ExternalTask/ExternalTaskService.cs
+++ Camunda.Api.Client/ExternalTask/ExternalTaskService.cs
@@ -13,45 +13,45 @@ namespace Camunda.Api.Client.ExternalTask
 
         internal ExternalTaskService(IExternalTaskRestService api) { _api = api; }
 
-        public QueryResource<ExternalTaskQuery, ExternalTaskInfo> Query(ExternalTaskQuery query = null) =>
+        public virtual QueryResource<ExternalTaskQuery, ExternalTaskInfo> Query(ExternalTaskQuery query = null) =>
             new QueryResource<ExternalTaskQuery, ExternalTaskInfo>(_api, query);
 
         /// <param name="externalTaskId">The id of the external task to be retrieved.</param>
-        public ExternalTaskResource this[string externalTaskId] => new ExternalTaskResource(_api, externalTaskId);
+        public virtual ExternalTaskResource this[string externalTaskId] => new ExternalTaskResource(_api, externalTaskId);
         
         /// <summary>
         /// Fetches and locks a specific number of external tasks for execution by a worker. Query can be restricted to specific task topics and for each task topic an individual lock time can be provided.
         /// </summary>
-        public Task<List<LockedExternalTask>> FetchAndLock(FetchExternalTasks fetching) => _api.FetchAndLock(fetching);
+        public virtual Task<List<LockedExternalTask>> FetchAndLock(FetchExternalTasks fetching) => _api.FetchAndLock(fetching);
 
         /// <summary>
         /// Set a number of retries for the external task.
         /// </summary>
-        public Task SetRetries(string externalTaskId, RetriesInfo retriesInfo) => _api.SetRetries(externalTaskId, retriesInfo);
+        public virtual Task SetRetries(string externalTaskId, RetriesInfo retriesInfo) => _api.SetRetries(externalTaskId, retriesInfo);
 
         /// <summary>
         /// Set a number of retries for the external task.
         /// </summary>
-        public Task SetPriority(string externalTaskId, PriorityInfo priorityInfo) => _api.SetPriority(externalTaskId, priorityInfo);
+        public virtual Task SetPriority(string externalTaskId, PriorityInfo priorityInfo) => _api.SetPriority(externalTaskId, priorityInfo);
         
         /// <summary>
         /// Completes a specific external task.
         /// </summary>
-        public Task Complete(string externalTaskId, CompleteExternalTask completeExternalTask) => _api.Complete(externalTaskId, completeExternalTask);
+        public virtual Task Complete(string externalTaskId, CompleteExternalTask completeExternalTask) => _api.Complete(externalTaskId, completeExternalTask);
 
         /// <summary>
         /// Sends a failure to a specific external task.
         /// </summary>
-        public Task HandleFailure(string externalTaskId, ExternalTaskFailure externalTaskFailure) => _api.HandleFailure(externalTaskId, externalTaskFailure);
+        public virtual Task HandleFailure(string externalTaskId, ExternalTaskFailure externalTaskFailure) => _api.HandleFailure(externalTaskId, externalTaskFailure);
 
         /// <summary>
         /// Sends a failure to a specific external task.
         /// </summary>
-        public Task HandleBpmnError(string externalTaskId, ExternalTaskBpmnError externalTaskBpmnError) => _api.HandleBpmnError(externalTaskId, externalTaskBpmnError);
+        public virtual Task HandleBpmnError(string externalTaskId, ExternalTaskBpmnError externalTaskBpmnError) => _api.HandleBpmnError(externalTaskId, externalTaskBpmnError);
 
         /// <summary>
         /// Unlocks a specific external task.
         /// </summary>
-        public Task Unlock(string externalTaskId) => _api.Unlock(externalTaskId);
+        public virtual Task Unlock(string externalTaskId) => _api.Unlock(externalTaskId);
     }
 }
